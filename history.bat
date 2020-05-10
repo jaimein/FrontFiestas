0eaf1e1fb4a184a2ec8edb59810b6ce1bdea05d5
JAIME ISERTE NAVARRO <jaimeiserte@hotmail.com>
2020-05-10 19:35:10 +0200

    creamos vista principal y personalizamos

--- history.bat
+++ history.bat
@@ -2,3 +2,5 @@
 ng add @angular/material
 ::creamos el componente nav que tendra tambien el routing
 ng g @angular/material:nav shell
+::creamos la ventana home, en la cual estara fiestas grupos poblaciones
+ng g @angular/material:dashboard home
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  /** Based on the screen size, switch from standard to one column per row */
+  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+    map(({ matches }) => {
+      if (matches) {
+        return [
+          { title: 'Fiestas', cols: 1, rows: 1 },
+          { title: 'Poblaciones', cols: 1, rows: 1 },
+          { title: 'Grupos', cols: 1, rows: 1 },
+          { title: 'Tipos', cols: 1, rows: 1 }
+        ];
+      }
+
+      return [
+        { title: 'Fiestas', cols: 2, rows: 1 },
+        { title: 'Poblaciones', cols: 1, rows: 1 },
+        { title: 'Grupos', cols: 1, rows: 2 },
+        { title: 'Tipos', cols: 1, rows: 1 }
+      ];
+    })
+  );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+}
