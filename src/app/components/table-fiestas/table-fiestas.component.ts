f30524a9e43d8390ec41202aa85afee74064a90a
JAIME ISERTE NAVARRO <jaimeiserte@hotmail.com>
2020-05-13 20:47:50 +0200

    tabla-fiestas ajustada para paginar y ordenar

--- src/app/components/table-fiestas/table-fiestas.component.ts
+++ src/app/components/table-fiestas/table-fiestas.component.ts
@@ -1,35 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FiestasService } from '../../services/fiestas.service';
 import { Observable } from 'rxjs';
 import { DataSource } from '@angular/cdk/collections';
 import { FiestaNombres } from '../../models/fiestas.model';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTable, MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-table-fiestas',
   templateUrl: './table-fiestas.component.html',
-  styleUrls: ['./table-fiestas.component.css']
+  styleUrls: ['./table-fiestas.component.css'],
 })
 export class TableFiestasComponent implements OnInit {
+  dataSource = new MatTableDataSource<FiestaNombres>();
+  displayedColumns = [
+    'fecha',
+    'nombreGrupo',
+    'nombreTipo',
+    'localidad',
+    'zona',
+  ];
 
-  dataSource = new FiestaNombresDataSource(this.fiestasService);
-  displayedColumns = ['zona', 'fecha', 'nombreGrupo', 'nombreTipo', 'localidad'];
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatTable) table: MatTable<FiestaNombres>;
 
-  constructor(private fiestasService: FiestasService) { }
+  constructor(private fiestasService: FiestasService) {}
 
   ngOnInit(): void {
+    this.fiestasService.getFiestasNombres().subscribe(things => {
+      this.dataSource.data = things;
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+  });
+
   }
 
 }
 
-
-export class FiestaNombresDataSource extends DataSource<any>{
-  constructor(private fiestaService: FiestasService){
-    super();
-  }
-
-  connect(): Observable<FiestaNombres[]>{
-    return this.fiestaService.getFiestasNombres();
-  }
-
-  disconnect() {}
-}
